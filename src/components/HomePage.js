230b57bbd5c51e3e7eed63538d8c334c9566b25c
evc2bds <91164928+evc2bds@users.noreply.github.com>
2023-07-06 10:21:12 +0200

    video on homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import Navbar from '../common/Navbar'
-import ServiceCards from './homepageCards/ServiceCards'
-import CoverCard from './homepageCards/CoverCard'
-import ForCustomersCard from './homepageCards/ForCustomersCard'
-import EventCards from './homepageCards/EventCards'
-import ProvideServiceCard from './homepageCards/ProvideServiceCard'
-import FAQ from './homepageCards/FAQ'
+import React from "react";
+import Navbar from "../common/Navbar";
+import ServiceCards from "./homepageCards/ServiceCards";
+import CoverCard from "./homepageCards/CoverCard";
+import ForCustomersCard from "./homepageCards/ForCustomersCard";
+import EventCards from "./homepageCards/EventCards";
+import ProvideServiceCard from "./homepageCards/ProvideServiceCard";
+import FAQ from "./homepageCards/FAQ";
 
 export default function HomePage(props) {
   return (
     <div>
       <Navbar></Navbar>
 
-        <CoverCard></CoverCard>
-        <br></br>
-        <ForCustomersCard></ForCustomersCard>
-        <ServiceCards></ServiceCards>
-        {/* <hr></hr> */}
-        <EventCards></EventCards>
-        <ProvideServiceCard></ProvideServiceCard>
-        <FAQ></FAQ>
+      <CoverCard></CoverCard>
+      <br></br>
+      <ForCustomersCard></ForCustomersCard>
+      <ServiceCards></ServiceCards>
+      <EventCards></EventCards>
+      <ProvideServiceCard></ProvideServiceCard>
+      <p className="hr"></p>
+
+      <p className="p service-header">
+        <span className="purple-header"> Primlancers </span> At Work
+      </p>
+      <div className="container-prov-serv m-9">
+        <iframe
+          className="iframe"
+          width="840"
+          height="472"
+          src="https://www.youtube.com/embed/SRIv2ziiO2Q"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <p className="hr"></p>
+
+      <FAQ></FAQ>
 
     </div>
-  )
+  );
 }
